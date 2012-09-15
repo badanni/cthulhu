adc55965e806b31c8b875391b360c0ce53d6e2fe
badanni <dannyvasconeze@gmail.com>
2012-09-15 00:25:40 -0500

    al dia

--- ideas/web_server.py
+++ ideas/web_server.py
@@ -27,12 +27,26 @@ Doesn't handle POST requests.
 """
 import SocketServer
 import SimpleHTTPServer
+from cliente_lib import *
 
+
+#Parametros del servidor web
 PORT = 8081
 
-def move():
+def move(a,b):
     """ sample function to be called via a URL"""
-    return 'hi'
+    #Inicio de ARIA y conexion con servidor del robot
+    aa = cliente_lib()
+    #aa.ip="192.168.2.45"
+    cliente = aa.cliente_inicio()
+    
+    TransRatio,RotRatio,LatRatio = [50*a,50*b,0]
+    cliente=aa.envio_ratioDrive(cliente,TransRatio,RotRatio,LatRatio)
+    
+    #apaga cliente
+    aa.cliente_apaga(cliente)
+    
+    return 'hi'+str(a)+" "+str(b)
 def adelante():
     return 'vamos!!!'
 def prueba():
@@ -41,24 +55,40 @@ def prueba():
     return valor
 
 class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
     def do_GET(self):
+        
         #Sample values in self for URL: http://localhost:8080/jsxmlrpc-0.3/
         #self.path  '/jsxmlrpc-0.3/'
         #self.raw_requestline   'GET /jsxmlrpc-0.3/ HTTP/1.1rn'
         #self.client_address    ('127.0.0.1', 3727)
-        if self.path=='/move':
+        if self.path=='/atras':
             #This URL will trigger our sample function and send what it returns back to the browser
-            self.send_response(200)
+            self.send_response(200) #200
             self.send_header('Content-type','text/html')
             self.end_headers()
-            self.wfile.write(move()) #call sample function here
+            self.wfile.write(move(-1,0)) #call sample function here
             return
         elif self.path=='/adelante':
             #This URL will trigger our sample function and send what it returns back to the browser
+            self.send_response(20)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(move(1,0)) #call sample function here
+            return
+        elif self.path=='/derecha':
+            #This URL will trigger our sample function and send what it returns back to the browser
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
-            self.wfile.write(adelante()) #call sample function here
+            self.wfile.write(move(0,1)) #call sample function here
+            return
+        elif self.path=='/izquierda':
+            #This URL will trigger our sample function and send what it returns back to the browser
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(move(0,-1)) #call sample function here
             return
         elif self.path=='/prueba':
             #This URL will trigger our sample function and send what it returns back to the browser
@@ -71,12 +101,8 @@ class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             #serve files, and directory listings by following self.path from
             #current working directory
             SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        self.wfile.write(adelante()) #call sample function here
-        return
+
+Aria.init()
 
 httpd = SocketServer.ThreadingTCPServer(("192.168.2.104", PORT),CustomHandler)
 
